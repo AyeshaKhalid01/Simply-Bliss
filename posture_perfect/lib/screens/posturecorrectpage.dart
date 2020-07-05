ffbbb8840b8bc03672de8569cbab82ed686ceab9
Haripriya Baskaran <b6haripriya@gmail.com>
2020-07-05 19:32:47 +0530

    Final commit

--- posture_perfect/lib/screens/posturecorrectpage.dart
+++ posture_perfect/lib/screens/posturecorrectpage.dart
@@ -1,11 +1,8 @@
+import 'dart:async';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:percent_indicator/linear_percent_indicator.dart';
-import 'package:posture_perfect/helpers/app_helper.dart';
-import 'package:posture_perfect/helpers/camera_helper.dart';
-import 'package:posture_perfect/helpers/tflite_helper.dart';
-import 'package:posture_perfect/models/result.dart';
-import 'package:tflite/tflite.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class PostureCorrectPage extends StatelessWidget {
   @override
@@ -31,145 +28,31 @@ class Posture extends StatefulWidget {
   @override
   _PostureState createState() => _PostureState();
 }
-Future<String> loadModel() async{  return Tflite.loadModel(
-model: "assets/yoga_classifier.tflite",
-labels: "assets/labels.txt",
-);
-}
 class _PostureState extends State<Posture> with TickerProviderStateMixin{
-  AnimationController _colorAnimController;
-  Animation _colorTween;
-
-  List<Result> outputs;
-  void initState() {
-      super.initState();
-
-      //Load TFLite Model
-      TFLiteHelper.loadModel().then((value) {
-        setState(() {
-          TFLiteHelper.modelLoaded = true;
-        });
-      });
-
-      //Initialize Camera
-      CameraHelper.initializeCamera();
-
-      //Setup Animation
-      _setupAnimation();
-
-      //Subscribe to TFLite's Classify events
-      TFLiteHelper.tfLiteResultsController.stream.listen((value) {
-        value.forEach((element) {
-          _colorAnimController.animateTo(element.confidence,
-              curve: Curves.bounceIn, duration: Duration(milliseconds: 500));
-        });
-
-        //Set Results
-        outputs = value;
-
-        //Update results on screen
-        setState(() {
-          //Set bit to false to allow detection again
-          CameraHelper.isDetecting = false;
-        });
-      }, onDone: () {
-
-      }, onError: (error) {
-        AppHelper.log("listen", error);
-      });
-    }
 
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
+    Completer<WebViewController> _controller = Completer<WebViewController>();
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Posture Detection'),
-      ),
-      body: FutureBuilder<void>(
-        future: CameraHelper.initializeControllerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            // If the Future is complete, display the preview.
-            return Stack(
-              children: <Widget>[
-                CameraPreview(CameraHelper.camera),
-                _buildResultsWidget(width, outputs)
-              ],
-            );
-          } else {
-            // Otherwise, display a loading indicator.
-            return Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
+      body: Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  child: WebView(
+                    initialUrl:  "https://teachablemachine.withgoogle.com/models/BQgy8F-lX/",
+                    javascriptMode: JavascriptMode.unrestricted,
+                    onWebViewCreated: (WebViewController webViewController){
+                      _controller.complete(webViewController);
+                    },
+                    onPageStarted: (String url) {
+                      print('Page started loading: $url');
+                    },
+                    onPageFinished: (String url) {
+                      print('Page finished loading: $url');
+                    },
+                    gestureNavigationEnabled: true,
+                  ),
+                ),
     );
-
 }
-  @override
-  void dispose() {
-    TFLiteHelper.disposeModel();
-    CameraHelper.camera.dispose();
-    AppHelper.log("dispose", "Clear resources.");
-    super.dispose();
-  }
-  Widget _buildResultsWidget(double width, List<Result> outputs) {
-    return Positioned.fill(
-      child: Align(
-        alignment: Alignment.bottomCenter,
-        child: Container(
-          height: 200.0,
-          width: width,
-          color: Colors.white,
-          child: outputs != null && outputs.isNotEmpty
-              ? ListView.builder(
-              itemCount: outputs.length,
-              shrinkWrap: true,
-              padding: const EdgeInsets.all(20.0),
-              itemBuilder: (BuildContext context, int index) {
-                return Column(
-                  children: <Widget>[
-                    Text(
-                      outputs[index].label,
-                      style: TextStyle(
-                        color: _colorTween.value,
-                        fontSize: 20.0,
-                      ),
-                    ),
-                    AnimatedBuilder(
-                        animation: _colorAnimController,
-                        builder: (context, child) => LinearPercentIndicator(
-                          width: width * 0.88,
-                          lineHeight: 14.0,
-                          percent: outputs[index].confidence,
-                          progressColor: _colorTween.value,
-                        )),
-                    Text(
-                      "${(outputs[index].confidence * 100.0).toStringAsFixed(2)} %",
-                      style: TextStyle(
-                        color: _colorTween.value,
-                        fontSize: 16.0,
-                      ),
-                    ),
-                  ],
-                );
-              })
-              : Center(
-              child: Text("Wating for model to detect..",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 20.0,
-                  ))),
-        ),
-      ),
-    );
-  }
-
-  void _setupAnimation() {
-    _colorAnimController =
-        AnimationController(vsync: this, duration: Duration(milliseconds: 500));
-    _colorTween = ColorTween(begin: Colors.green, end: Colors.red)
-        .animate(_colorAnimController);
-  }
 }
 
